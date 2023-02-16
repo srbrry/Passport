a68c22d42b3860b5fb48fb7f55bc9016e74ee6dc
vb <vaughnbernard16@gmail.com>
2023-02-16 10:56:28 -0500

    added location and note models to database

---
+++ models/notes.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    } 
+}, {
+    timestamps: true
+})
+
+module.exports = noteSchema
