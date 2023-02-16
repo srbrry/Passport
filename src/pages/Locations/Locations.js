f56d3a1465c89674eee9b1200c84a694b400cec6
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-02-16 12:28:15 -0500

    location form and button toggle

--- src/pages/Locations/Locations.js
+++ src/pages/Locations/Locations.js
@@ -1,5 +1,21 @@
-export default function NewOrderPage() {
+import { useState } from "react"
+import LocationForm from "../../components/LocationForm/LocationForm";
+
+export default function LocationPage() {
+
+    const [showForm, setShowForm] = useState(false)
+
     return(
-        <h2>My Destinations</h2>
+        <>
+         <h2>My Destinations</h2>
+       
+         <button onClick={() => setShowForm(!showForm)}>Add Location</button> 
+        
+        {showForm ? (<LocationForm/>) : " "}
+
+        </>
+       
     )
 }
+
+
