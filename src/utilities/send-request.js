1fed321f5e2c5551ba8fb46c0b20d3c08e17c3bd
Sara Abdelbarry <sarabarry@Saras-MacBook-Pro-2.local>
2023-02-17 11:13:26 -0500

    corrected spelling

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -12,7 +12,7 @@ export default async function sendRequest(url, method="GET", payload=null) {
         //make sure we have headers on our options
         options.headers = options.headers || {}
         //add in our token with an authorization header
-        options.headers.Autorization = `Bearer ${token}`
+        options.headers.Authorization = `Bearer ${token}`
         //make sure you capitalize Authorization
         //best practice is to begin with "Bearer "
     }
