4860c32d2057b7505a208200e7d2507ac3e4dfc1
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-02-21 09:04:01 -0500

    note toggle

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -1,7 +1,16 @@
-export default function NoteCard({ note }){
+import { useState } from "react"
+import UpdateNoteForm from "../updateNotesForm/updateNotesForm";
+
+export default function NoteCard({ notes, data }){
+
+    const [formToggle, setFormToggle] = useState(false)
+
     return(
         <> 
-            <h2>{note.title}</h2>
+            <h2><span onClick={() => setFormToggle(!formToggle)}>{notes.title}</span></h2>
+
+            {formToggle ? (<UpdateNoteForm notes={notes} data={data} />) : ""}
+            
         </>
     )
 }
